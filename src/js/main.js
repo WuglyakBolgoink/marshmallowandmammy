195c0ef757b593ebf409f3dad989ff7affcd88c1
mike-griffin87 <michaelgriffin87@me.com>
2016-06-06 03:30:48 +0100

    week 6 content dummy content added.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,16 @@
 angular.module('marshmallowAndMammy',['ngRoute'])
 .controller('mmController', ['$scope', '$http', MmController])
-.config(function($routeProvider){
+.config(['$routeProvider', function($routeProvider){
   $routeProvider.
-    when("/", {templateUrl:"/partials/homepage.html"});
-});
+    when("/", {templateUrl:"/partials/homepage.html"}).
+    when("/weeks5-8.html", {templateUrl:"/partials/week5-8.html"});
+}]);
 
 function MmController ($scope, $http){
   $http.get('trimester-info.json').then(function(trimesterData){
     $scope.weekPanels = trimesterData.data;
     $scope.weekTotal = $scope.weekPanels.length;
-  });
+  }, function(e){console.log(e.data);});
 }
 
 
