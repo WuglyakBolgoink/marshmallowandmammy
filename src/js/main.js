37517b1759317ad78c8c58a721f7d94ea47e0dc5
mike-griffin87 <michaelgriffin87@me.com>
2016-06-06 19:34:55 +0100

    bug fixes. Angular edits.
    removed # from urls. added some ng-click functions.

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,32 @@
 angular.module('marshmallowAndMammy',['ngRoute'])
-.controller('mmController', ['$scope', '$http', MmController])
-.config(['$routeProvider', function($routeProvider){
+.controller('mmController', ['$scope', '$http', '$location', MmController])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $routeProvider.
     when("/", {templateUrl:"/partials/homepage.html"}).
     when("/weeks5-8.html", {templateUrl:"/partials/week5-8.html"});
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+
 }]);
 
-function MmController ($scope, $http){
+function MmController ($scope, $http, $location){
   $http.get('trimester-info.json').then(function(trimesterData){
     $scope.weekPanels = trimesterData.data;
     $scope.weekTotal = $scope.weekPanels.length;
   }, function(e){console.log(e.data);});
-}
 
+  $scope.go = function(path) {
+    if(path !== 'inactive'){
+      $location.path(path);
+    }
+  };
 
-$(document).ready(function(){
-
-  $('nav').on('click', function(){
-    $('.hamburger').toggleClass('menu-is-showing');
+  $scope.toggleNav = function(){
     $('.main-nav').slideToggle('fast');
-  });
+    $('.hamburger').toggleClass('menu-is-showing');
+  };
 
-
-});
+}
